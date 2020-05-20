d821b91c0e869e29128bc28f29f49dfe8703eac2
Thomas Hipp <thomas.hipp@canonical.com>
2020-05-20 19:03:35 +0200

    generators/cloud-init: Add IPv6 to network config
    This adds IPv6 to the cloud-init network config.
    
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- generators/cloud-init.go
+++ generators/cloud-init.go
@@ -124,9 +124,13 @@ local-hostname: {{ container.name }}
 config:
   - type: physical
     name: {% if instance.type == "virtual-machine" %}enp5s0{% else %}eth0{% endif %}
-    subnets:
-      - type: {% if config_get("user.network_mode", "") == "link-local" %}manual{% else %}dhcp{% endif %}
-        control: auto{% else %}{{ config_get("user.network-config", "") }}{% endif %}
+    subnets:{% if config_get("user.network_mode", "") == "link-local" %}
+      - type: manual
+        control: auto{% else %}
+      - type: dhcp
+        control: auto
+      - type: dhcp6
+        control: auto{% endif %}{% else %}{{ config_get("user.network-config", "") }}{% endif %}
 `
 	default:
 		return fmt.Errorf("Unknown cloud-init configuration: %s", defFile.Name)
