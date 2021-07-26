8803ca65481adf65849e615ea228341a1613fefc
Thomas Hipp <thomas.hipp@canonical.com>
2021-07-26 17:40:32 +0200

    main: Wrap overlay errors
    Signed-off-by: Thomas Hipp <thomas.hipp@canonical.com>

--- distrobuilder/chroot.go
+++ distrobuilder/chroot.go
@@ -31,24 +31,24 @@ func getOverlay(logger *zap.SugaredLogger, cacheDir, sourceDir string) (func(), 
 
 	err := os.Mkdir(upperDir, 0755)
 	if err != nil {
-		return nil, "", err
+		return nil, "", errors.Wrapf(err, "Failed to create directory %q", upperDir)
 	}
 
 	err = os.Mkdir(overlayDir, 0755)
 	if err != nil {
-		return nil, "", err
+		return nil, "", errors.Wrapf(err, "Failed to create directory %q", overlayDir)
 	}
 
 	err = os.Mkdir(workDir, 0755)
 	if err != nil {
-		return nil, "", err
+		return nil, "", errors.Wrapf(err, "Failed to create directory %q", workDir)
 	}
 
 	opts := fmt.Sprintf("lowerdir=%s,upperdir=%s,workdir=%s", sourceDir, upperDir, workDir)
 
 	err = unix.Mount("overlay", overlayDir, "overlay", 0, opts)
 	if err != nil {
-		return nil, "", err
+		return nil, "", errors.Wrap(err, "Failed to mount overlay")
 	}
 
 	cleanup := func() {
