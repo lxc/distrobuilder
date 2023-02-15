664d32d319b9f9337272df6bd9b2638a156f3c6a
Ruth Fuchss <ruth.fuchss@canonical.com>
2023-02-15 15:54:59 +0100

    doc: basic Sphinx setup
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

---
+++ .sphinx/.markdownlint/doc-lint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -eu
+
+if ! command -v mdl >/dev/null; then
+    echo "Install mdl with 'snap install mdl' first."
+    exit 1
+fi
+
+trap "rm -rf .tmp/" EXIT
+
+## Preprocessing
+
+for fn in $(find doc/ -name '*.md'); do
+    mkdir -p $(dirname ".tmp/$fn");
+    sed -E "s/(\(.+\)=)/\1\n/" $fn > .tmp/$fn;
+done
+
+mdl .tmp/doc -s.sphinx/.markdownlint/style.rb -u.sphinx/.markdownlint/rules.rb --ignore-front-matter > .tmp/errors.txt || true
+
+## Postprocessing
+
+filtered_errors="$(grep -vxFf .sphinx/.markdownlint/exceptions.txt .tmp/errors.txt)"
+if [ "$(echo "$filtered_errors" | wc -l)" = "2" ]; then
+    echo "Passed!"
+    exit 0
+else
+    echo "Failed!"
+    echo "$filtered_errors"
+    exit 1
+fi
---
+++ doc/readme.md
@@ -0,0 +1,6 @@
+# Readme
+
+% Include content from [../README.md](../README.md)
+```{include} ../README.md
+   :start-line: 2
+```
