b8a93a357c51d185881b92b8a84cb85f566eb65d
Valery Ushakov <valery.ushakov@bell-sw.com>
2025-02-18 03:51:34 +0300

    sources: Add Alpaquita downloader
    Signed-off-by: Valery Ushakov <valery.ushakov@bell-sw.com>

---
+++ sources/alpaquita-http.go
@@ -0,0 +1,75 @@
+package sources
+
+import (
+	"crypto/sha512"
+	"fmt"
+	"path/filepath"
+
+	"github.com/lxc/distrobuilder/shared"
+)
+
+type alpaquita struct {
+	common
+}
+
+func (s *alpaquita) Run() error {
+	baseURL, fname, err := s.getMiniroot(s.definition)
+	if err != nil {
+		return err
+	}
+
+	var fpath string
+	tarballURL := baseURL + fname
+	if s.definition.Source.SkipVerification {
+		fpath, err = s.DownloadHash(s.definition.Image,
+			tarballURL, "", nil)
+		if err != nil {
+			return err
+		}
+	} else {
+		fpath, err = s.DownloadHash(s.definition.Image,
+			tarballURL, tarballURL+".sha512", sha512.New())
+		if err != nil {
+			return err
+		}
+	}
+
+	tarballLocal := filepath.Join(fpath, fname)
+	s.logger.WithField("file", tarballLocal).Info("Unpacking image")
+
+	err = shared.Unpack(tarballLocal, s.rootfsDir)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Sample URLs (or with "latest" instead of date):
+//
+//	https://packages.bell-sw.com/alpaquita/musl/stream/releases/x86_64/alpaquita-minirootfs-stream-241231-musl-x86_64.tar.gz
+//	https://packages.bell-sw.com/alpaquita/glibc/23/releases/aarch64/alpaquita-minirootfs-23-241231-glibc-aarch64.tar.gz
+func (s *alpaquita) getMiniroot(definition shared.Definition) (string, string, error) {
+	// default server
+	if s.definition.Source.URL == "" {
+		s.definition.Source.URL = "https://packages.bell-sw.com"
+	}
+
+	// require explicit variant
+	if s.definition.Image.Variant == "default" {
+		return "", "", fmt.Errorf("Alpaquita requires explicitly specified variant")
+	}
+
+	base := fmt.Sprintf("%s/alpaquita/%s/%s/releases/%s/",
+		s.definition.Source.URL,
+		s.definition.Image.Variant,
+		s.definition.Image.Release,
+		s.definition.Image.ArchitectureMapped)
+
+	fname := fmt.Sprintf("alpaquita-minirootfs-%s-latest-%s-%s.tar.gz",
+		s.definition.Image.Release,
+		s.definition.Image.Variant,
+		s.definition.Image.ArchitectureMapped)
+
+	return base, fname, nil
+}
