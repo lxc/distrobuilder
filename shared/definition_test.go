bc1dd4f8e212c851c26893a83ddf344fa8275c63
Stéphane Graber <stgraber@stgraber.org>
2025-04-28 22:37:53 -0400

    Revert "Replace `gopkg.in/yaml.v2` with `github.com/goccy/go-yaml`"
    This reverts commit 50d870b0255453d4f4d8ce035793ba0ce17d0d68.
    
    We have seen widespread image build failures following this change.
    Clearly this alternative YAML parser isn't one for one compatible with
    our current one.
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/definition_test.go
+++ shared/definition_test.go
@@ -4,9 +4,9 @@ import (
 	"log"
 	"testing"
 
-	"github.com/goccy/go-yaml"
 	"github.com/lxc/incus/v6/shared/osarch"
 	"github.com/stretchr/testify/require"
+	yaml "gopkg.in/yaml.v2"
 )
 
 func TestSetDefinitionDefaults(t *testing.T) {
